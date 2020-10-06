81796cd6686c00e26ca80ee0e7c1fd1365dee20d
Alireza <alireza.amirshahi@epfl.ch>
2020-10-06 19:09:29 +0200

    convert the results to a json file

--- utils/results_to_json.py
+++ utils/results_to_json.py
@@ -13,11 +13,22 @@ for idx, l in enumerate(content):
     elif l.startswith('orig'):
         patient_list_end.append(idx)
 
+exp = []
 for start, end in zip(patient_list_start, patient_list_end):
     patient_list = []
     for patient_line in range(start,end):
         patient_list.append(content[patient_line].strip())
     as_list = ast.literal_eval(''.join(patient_list))
-    print(as_list)
 
+    result_line = content[end]
+    orig_end_index = result_line.find(',')
+    orig = float(result_line[10:orig_end_index])
+    synt = float(result_line[orig_end_index + 13: -1])
+
+    exp.append({'patients' : as_list,
+                'orig': orig,
+                'synt': synt})
+
+with open('../results/final_results.json', 'w') as outfile:
+    json.dump(exp, outfile)
 
